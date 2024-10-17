100518eb33c011af82ec605cbd9f24c926d7b93c
NguyenDucTam <ITCSIU21031@student.hcmiu.edu.vn>
2024-10-17 18:40:32 +0700

    Set up database and connect to server
    Database: v-academy
    SQL tool: phpMyAdmin - Mysql

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,31 @@
 const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const colors = require("colors");
+const morgan = require("morgan");
+const db = require("./src/config/db");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(morgan("dev"));
+app.use(express.json());
+// Middleware to parse incoming requests with JSON payloads
+app.use(bodyParser.json());
+
+
+//config .env
+dotenv.config();
+
+
+//test app
 app.get("/", (req, res) => {
-  return res.send("Hello World");
+  return res.status(200).send("<h1>Hello World</h1>");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`.bgMagenta.white);
 });
+
+//routes
+app.use("/api/students", require("./src/routes/studentRoutes"));

27735eb2ef2629dc51997de120388c09aaebe0e6
Binh Duong Pham <binhduong.pham2405@gmail.com>
2024-12-01 11:37:09 +0700

    addRoute
    addRoute

--- backend/src/routes/tutorRoute.js
+++ backend/src/routes/tutorRoute.js
@@ -12,5 +12,11 @@ router.get("/tutorInfo/:id", verifyToken, tutorController.tutorGetInfo);
 router.get("/tutorLessonClass/:class_id", verifyToken, tutorController.tutorGetLessonClass);
 router.get("/tutorClassDetail/:class_id", verifyToken, tutorController.tutorGetClassDetail);
 router.get("/tutorLessonDetail/:lesson_id", verifyToken, tutorController.tutorGetLessonDetail);
+// Route to add a new lesson
+router.post('/lesson', tutorController.createLesson);
+  // Route to get the newest lesson
+router.get('/lesson/latest', tutorController.getNewest);
+  // Route to add a lesson to a class
+router.post('/lesson/class', tutorController.addLessonToClassController);
 
 module.exports = router;

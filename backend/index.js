8bbfd3cb58fd0025363715aeb79cafa371a505f8
NguyenDucTam <ITCSIU21031@student.hcmiu.edu.vn>
2024-12-18 22:51:55 +0700

    config server

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 // Use CORS middleware
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow specific origin
+    origin: "https://v-academy.onrender.com", // Allow specific origin
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", // Allow specific HTTP methods
     allowedHeaders: "Content-Type,Authorization", // Allow specific headers
     credentials: true, // Allow cookies or other credentials
